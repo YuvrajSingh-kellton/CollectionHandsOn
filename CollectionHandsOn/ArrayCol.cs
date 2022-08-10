a36b7a1e7feeabd750802f3b180c7040d25a50d2
YuvrajSingh-kellton <105622006+YuvrajSingh-kellton@users.noreply.github.com>
2022-08-10 09:44:02 +0530

    Initial Commit

---
+++ CollectionHandsOn/ArrayCol.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace CollectionHandsOn
+{
+    public class ArrayCol
+    {
+        public void CArray<T>(int input, string cases)
+        {
+            T[] arr = new T[input];
+            ArrayList obj = new ArrayList();
+            for (int i = 0; i < input; i++)
+            {
+                if (cases == "1")
+                {
+                    dynamic n = Convert.ToInt32(Console.Read());
+                    arr[i] = n;
+                }
+                else if (cases == "2")
+                {
+                    dynamic n = Console.Read();
+                    arr[i] = n;
+                }
+                else
+                {
+                    dynamic n = Convert.ToBoolean(Console.Read());
+                    arr[i] = n;
+                }
+            }
+            for (int i = 0; i < input; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            return;
+        }
+    }
+}
---
+++ CollectionHandsOn/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionHandsOn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        TOP:
+            int size;
+            Console.WriteLine("1. Create interger array to hold int values 0 to 9");
+            Console.WriteLine("2. Create array to hold names Tim, Martin, Nikki, & Sara");
+            Console.WriteLine("3. Create array of length 10 alternating between true & false, starting with true.");
+            Console.WriteLine("4. Create list of 5 icecream flavours");
+            string cases = Console.ReadLine();
+            ArrayCol obj = new ArrayCol();
+            switch (cases)
+            {
+                case "1":
+                    size = 10;
+                    obj.CArray<int>(size, cases);
+                    break;
+                case "2":
+                    size = 4;
+                    obj.CArray<string>(size, cases);
+                    break;
+                case "3":
+                    size = 10;
+                    obj.CArray<bool>(size, cases);
+                    break;
+                case "4":
+                    List<string> flavour = new List<string>();
+                    for (int i = 0; i < 5; i++)
+                    {
+                        flavour.Add(Console.ReadLine());
+                    }
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.WriteLine(flavour[i]);
+                    }
+                    Console.WriteLine(flavour.Count());
+                    flavour.RemoveAt(2);
+                    for (int i = 0; i < 4; i++)
+                    {
+                        Console.WriteLine(flavour[i]);
+                    }
+                    Console.WriteLine(flavour.Count());
+                    break;
+                default:
+                    Console.WriteLine("Entered wrong number please try again");
+                    goto TOP;
+            }
+        }
+    }
+}
